3a3980b0c1f34e3707f18234b4aec1d043ff0275
studiozandra <zan@studiozandra.com>
2019-05-09 17:24:16 -1000

    add get request from jsonplaceholder.typicode.com

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,31 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'; // headers for sending data in content type of JSON
+import { Todo } from '../models/Todo'; // the TS file which tells the proper format for each task
+import { Observable } from 'rxjs'; // short for Reactive eXtensions
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
 
-  constructor() { }
-  // this service feeds the tasks 
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: 'Task 1',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Task 2',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Task 3',
-        completed: false
-      }
-
-    ]
+  constructor(private http:HttpClient) { } // we can just say this.http.blahblah
+  
+  // this service feeds the tasks with a get request to the JSON Placeholder URL
+  getTodos():Observable<Todo[]> { // similar to making requests in React with Axios
+     return this.http.get<Todo[]>(this.todosUrl); // since we made the URL a property we access it using 'this'. 
+     
   }
 
 
